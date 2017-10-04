c4d66996b47cda7c266cd61c4a134739adb55642
MgI <mawguai@gmail.com>
2017-10-05 01:56:00 +0200

    add Assets folder

---
+++ Assets/Scripts/DetectLetter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectLetter : MonoBehaviour {
+
+    public GameObject letter;
+
+    void OnTriggerEnter(Collider col)
+    {
+        Debug.Log(col.gameObject.name + " collision detected");
+        if (col.gameObject == letter.gameObject)
+        {
+            Debug.Log(letter.gameObject.name + " Well placed");
+        }
+    }
+}
