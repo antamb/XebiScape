c5a4d017ee7ef176c9f761a54af13c1a95dc514b
sbuisson <sbuisson@xebia.fr>
2017-10-23 00:20:41 +0200

    Add server&Rack
    (cherry picked from commit 69c6daf)
    
    server& rack : script + texture
    
    (cherry picked from commit 7440305)
    
    server

---
+++ Assets/Scripts/DetectServer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectServer : MonoBehaviour {
+
+	bool placed=false;
+	bool placing=false;
+	Collider collided;
+	void OnTriggerEnter(Collider col)
+	{
+		Debug.Log(col.gameObject.name + " collision detected");
+		if (col.gameObject.CompareTag("ServerTag"))
+		{
+
+			collided = col;
+			placing = true;
+
+			if (Vector3.Distance(col.gameObject.transform.position, this.gameObject.transform.position )<1)
+			{
+				placed = true;
+				//placing = false;
+				Debug.Log(col.gameObject.name + " Well placed");
+
+			}
+
+		}
+	}
+
+	void Update(){
+		if (placing) {
+			float speed=2f;
+			Debug.Log(collided.gameObject.name + "  placing");
+			Debug.Log("destination"+this.gameObject.name+this.gameObject.transform.position+this.gameObject.transform.localPosition);
+			float step = speed * Time.deltaTime;
+			Vector3 destination =this.gameObject.transform.position;
+			//destination.Scale(this.gameObject.transform.transform.lossyScale);
+			collided.attachedRigidbody.velocity = new Vector3 (0,0,0);
+			collided.attachedRigidbody.angularVelocity =new  Vector3 (0,0,0);
+			Debug.Log(" "+collided.gameObject.transform.position +" "+destination+" "+ Vector3.MoveTowards(collided.gameObject.transform.position,  destination, step));
+			collided.gameObject.transform.position = Vector3.MoveTowards(collided.gameObject.transform.position,  destination, step);
+			collided.gameObject.transform.position = Vector3.RotateTowards(collided.gameObject.transform.position,destination, step, step);
+		}
+	}
+}
